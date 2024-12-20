a4508e08aa2a78541815132b6ef861cf367235b2
Christopher Young <christopheryoung@MacBook123.local>
2024-12-20 13:22:22 -0800

    added tables w/ test data

--- src/components/tables/ClockinTable.tsx
+++ src/components/tables/ClockinTable.tsx
@@ -1,10 +1,51 @@
 import "./ClockinTable.scss";
+// Bootstrap
+import Table from 'react-bootstrap/Table';
+
+interface IEmployee {
+  EID: string;
+  clockins: number;
+  lastClockin: Date;
+}
+
+const testData: IEmployee[] = [
+  {
+    EID: "12345",
+    clockins: 5,
+    lastClockin: new Date()
+  },
+  {
+    EID: "12346",
+    clockins: 5,
+    lastClockin: new Date()
+  },
+  {
+    EID: "12347",
+    clockins: 5,
+    lastClockin: new Date()
+  },
+];
 
 const ClockinTable = () => {
   return(
-    <div className="clockinTable">
-      Clockin Table
-    </div>
+    <Table className="clockinTable" striped bordered hover>
+      <thead>
+        <tr>
+          <th>EID</th>
+          <th>Clock-ins</th>
+          <th>Last Clock-in</th>
+        </tr>
+      </thead>
+      <tbody>
+        {testData.map((clockin, idx) => (
+          <tr key={idx}>
+            <td>{clockin.EID}</td>
+            <td>{clockin.clockins}</td>
+            <td>{clockin.lastClockin.toDateString()}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   )
 };
 
--- src/components/tables/EmployeesTable.tsx
+++ src/components/tables/EmployeesTable.tsx
@@ -1,10 +1,93 @@
 import "./EmployeesTable.scss";
+// Bootstrap
+import Table from 'react-bootstrap/Table';
+
+interface IEmployee {
+  EID: string;
+  firstName: string;
+  lastName: string;
+  startDate: Date;
+  salary: number;
+  risk: "low" | "mid" | "high";
+  remote: boolean;
+}
+
+const testData: IEmployee[] = [
+  {
+    EID: "12345",
+    firstName: "Bob",
+    lastName: "Bobby",
+    startDate: new Date(),
+    salary: 50000,
+    risk: "low",
+    remote: false
+  },
+  {
+    EID: "12346",
+    firstName: "Bob",
+    lastName: "Bobby",
+    startDate: new Date(),
+    salary: 50000,
+    risk: "low",
+    remote: false
+  },
+  {
+    EID: "12347",
+    firstName: "Bob",
+    lastName: "Bobby",
+    startDate: new Date(),
+    salary: 50000,
+    risk: "low",
+    remote: false
+  },
+  {
+    EID: "12348",
+    firstName: "Bob",
+    lastName: "Bobby",
+    startDate: new Date(),
+    salary: 50000,
+    risk: "low",
+    remote: false
+  },
+  {
+    EID: "12349",
+    firstName: "Bob",
+    lastName: "Bobby",
+    startDate: new Date(),
+    salary: 50000,
+    risk: "low",
+    remote: false
+  }
+];
 
 const EmployeesTable = () => {
   return(
-    <div className="employeesTable">
-      Employees Table
-    </div>
+    <Table className="employeesTable" striped bordered hover>
+      <thead>
+        <tr>
+          <th>EID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Start Date</th>
+          <th>Salary</th>
+          <th>Risk</th>
+          <th>Remote</th>
+        </tr>
+      </thead>
+      <tbody>
+        {testData.map((employee, idx) => (
+          <tr key={idx}>
+            <td>{employee.EID}</td>
+            <td>{employee.firstName}</td>
+            <td>{employee.lastName}</td>
+            <td>{employee.startDate.toDateString()}</td>
+            <td>{employee.salary}</td>
+            <td>{employee.risk}</td>
+            <td>{employee.remote ? "yes" : "no"}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
   )
 };
 
